fa8db4015991d05d201973e42691d7a37c55f5b3
Yurina Shin <yurinas@gmail.com>
2018-08-04 18:22:04 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+  @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
+
+  def call(env)
+    req = Rack::Request.new(env)
+    resp = Rack::Response.new
+
+    if req.path.match(/items/)
+      item_title = req.path.split(/items/).last
+      item = @@items.find{|a| a.name == item_title}
+
+      resp.write item.price
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
