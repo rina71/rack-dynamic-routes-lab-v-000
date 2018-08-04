44464fe222abb3226f68b49726e9610b8938f25e
Yurina Shin <yurinas@gmail.com>
2018-08-04 18:32:14 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+  @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
+
+  def call(env)
+    req = Rack::Request.new(env)
+    resp = Rack::Response.new
+
+    if req.path.match(/items/)
+      item_title = req.path.split("/items/").last
+       if item = @@items.find{|a| a.name == item_title}
+         resp.write item.price
+       else
+         resp.write "Item not found"
+         resp.status = 400
+       end
+     else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+
+end
--- app/item.rb
+++ app/item.rb
@@ -1,8 +1,11 @@
 class Item
   attr_accessor :name, :price
 
+
   def initialize(name,price)
     @name = name
     @price = price
+
   end
+
 end
